b3638ecc033f6ae5f6c79c64a8964fcf3bffd16d
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2026-01-02 15:41:40 +0200

    -

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,6 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'categories','as'=>'categories.'], function () {
-    Route::get('/', [CategoryController::class, 'index'])->name('index');
-    Route::get('create', [CategoryController::class, 'create'])->name('create');
-    // Route::get('{id}', [CategoryController::class, 'show'])->name('show');
-    Route::post('/', [CategoryController::class, 'store'])->name('store');
-    Route::get('{id}/edit', [CategoryController::class, 'edit'])->name('edit');
-    Route::put('{id}', [CategoryController::class, 'update'])->name('update');
+Route::get('/', function () {
+    return view('welcome');
 });
